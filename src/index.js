52f1e3fca847a7fdc320f47e9cfb24164a6452e4
manojsingh047 <manojsingh047@gmail.com>
2019-08-04 01:02:49 +0530

    addeed name

--- src/index.js
+++ src/index.js
@@ -7,32 +7,36 @@ const app = (() => {
   };
 
   const initBGColorChange = () => {
-    const appEle = document.getElementById("app");
+    const appEle = document.getElementById("bg");
     const OPACITY_RANGE = {
-      MIN : 3000,
-      MAX : 9500
-    }
+      MIN: 3000,
+      MAX: 8000
+    };
     const COUNTER_ACTIONS = {
       INCREMENT: "increment",
       DECREMENT: "decrement"
     };
     let floatNum = OPACITY_RANGE.MIN;
     let counterAction = COUNTER_ACTIONS.INCREMENT;
-    const bgTimeOutsetTimeout = setInterval(() => {
-      if (floatNum > OPACITY_RANGE.MAX || floatNum < OPACITY_RANGE.MIN) {
-        toggleCounterAction();
-      }
-      const value = getValue(counterAction);
-      const opacityValue = getOpacityValue(value);
-      appEle.style.opacity = `${opacityValue}`;
-    },100);
+    
+    
+    const bgSetInterval = () => {
+      setInterval(() => {
+        if (floatNum > OPACITY_RANGE.MAX || floatNum < OPACITY_RANGE.MIN) {
+          toggleCounterAction();
+        }
+        const value = getValue(counterAction);
+        const opacityValue = getOpacityValue(value);
+        appEle.style.opacity = `${opacityValue}`;
+      }, 100);
+    };
 
     const getValue = counterAction => {
-      const stepSize = 50;
-      if(counterAction === COUNTER_ACTIONS.INCREMENT){
+      const stepSize = 100;
+      if (counterAction === COUNTER_ACTIONS.INCREMENT) {
         floatNum += stepSize;
-      }else{
-        floatNum -= stepSize;    
+      } else {
+        floatNum -= stepSize;
       }
       return floatNum;
     };
@@ -47,6 +51,9 @@ const app = (() => {
           ? COUNTER_ACTIONS.DECREMENT
           : COUNTER_ACTIONS.INCREMENT;
     };
+
+    bgSetInterval();
+
   };
 
   return {
